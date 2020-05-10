9f6a4abeb6f83679a4f4fc231b3d0308398d1cff
KUNDAN KUMAR <kundanjnv11@gmail.com>
2020-05-10 13:05:57 +0530

    Update disjoint_set_union.cpp
    working code

--- Tree/disjoint_set_union.cpp
+++ Tree/disjoint_set_union.cpp
@@ -1,35 +1,55 @@
-class UnionFind{
-public:
-    
-    vector<int> parent;
-    vector<int> size;
+#include<iostream>
+#include<vector>
+#define  ll long long int
+#define MAX 100001
+using namespace std;
 
-    void init(){
-        parent.clear();
+vector<int> bit;
+int n;
+
+void init(int num){
+    bit.resize(n+1,0);
+    n = num;
+}
+
+int sum(int i){
+    int ret = 0;
+    while(i > 0){
+        ret += bit[i];
+        i -= i & (-i);
     }
+    return ret;
+}
 
-    void make_set(int n){
-        for(int i=0;i<n;++i){
-            parent.push_back(i);
-            size[i] = 1;
-        }
+int sum(int l,int r){
+    return sum(r) - sum(l-1);
+}
+
+void add(int delta,int i){
+    i++;
+
+    while(i <= n){
+        bit[i] += delta;
+        i += i & (-i);
     }
+}
 
-    int find_set(int v){
-        if(v == parent[v]) return v;
-        return parent[v] = find_set(parent[v]);
+int main(){
+    int m;
+    cin>>n>>m;
+
+    init(n);
+
+    vector<int> a(n);
+
+    for(int i=0;i<n;++i){
+        cin>>a[i];
+        add(a[i],i);
     }
-
-    void union_set(int u,int v){
-        int a = find_set(u);
-        int b = find_set(v);
-
-        if(a != b){
-            if(size[a] < size[b]){
-                swap(a,b);
-            }
-            parent[b] = a;
-            size[b] += size[a];
-        }
+    while(m--){
+        int l,r;
+        cin>>l>>r;
+        cout<<sum(l,r)<<endl;
     }
-};
+}
+
